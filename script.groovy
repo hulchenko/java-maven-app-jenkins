c963b6f048465ef1d6b6d9527394336d0f1436af
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-06-07 18:44:40 -0600

    update jenkinsfile with groovy functions

--- script.groovy
+++ script.groovy
@@ -1,14 +1,18 @@
-def buildApp(){
-    echo 'Hello from script groovy file! Building the application'
+def buildJar(){
+                    echo "building the application.."
+                    sh "mvn package"
 }
 
-def testApp(){
-    echo 'Testing the application'
+def buildImage(){
+     echo "building docker image.."
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                        sh 'docker build -t hulchenko/demo-app:jma-2.0 .'
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh 'docker push hulchenko/demo-app:jma-2.0'
 }
 
 def deployApp(){
     echo 'Deploying the application'
-    echo "Deploying version ${params.VERSION}"
 }
 
 return this
